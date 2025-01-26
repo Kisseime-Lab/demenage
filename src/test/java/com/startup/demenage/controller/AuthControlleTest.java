115da67c0fb831a20e517ec0c2fbbb350a42f8a0
Kyse7535 <ktevot@gmail.com>
2025-01-26 10:44:20 +0100

    add test

--- src/test/java/com/startup/demenage/controller/AuthControlleTest.java
+++ src/test/java/com/startup/demenage/controller/AuthControlleTest.java
@@ -1,7 +1,9 @@
 package com.startup.demenage.controller;
 
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +12,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.startup.demenage.controllers.AuthController;
 import com.startup.demenage.dto.UserDto;
 import com.startup.demenage.entity.UserEntity;
@@ -19,72 +22,92 @@ import com.startup.demenage.model.SignedInUser;
 import com.startup.demenage.service.UserService;
 import com.startup.demenage.service.serviceImpl.UserServiceImpl;
 
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.Instant;
+
 import static org.hamcrest.core.Is.is;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthControlleTest {
 
-    static MockMvc mockMvc;
+    MockMvc mockMvc;
 
-    private static UserServiceImpl service = mock(UserServiceImpl.class);
-    private static UserDto userDto = mock(UserDto.class);
-    private static PasswordEncoder passwordEncoder = mock(PasswordEncoder.class);
-    private static AuthController controller;
-    private static UserEntity userEntity = new UserEntity();
-    private static SignInReq signInReq = new SignInReq();
-    private static SignedInUser signedInUser = new SignedInUser();
+    @Mock
+    private UserServiceImpl service;
+    @Mock
+    private UserDto userDto;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @InjectMocks
+    private  AuthController controller;
+    private UserEntity userEntity = new UserEntity();
+    private SignInReq signInReq = new SignInReq();
+    private SignedInUser signedInUser = new SignedInUser();
 
-    // @BeforeAll
-    // public static void setup() {
-    //     controller = new AuthController(service, passwordEncoder, userDto);
-    //     mockMvc = MockMvcBuilders.standaloneSetup(controller)
-    //     .setControllerAdvice(new GlobalCatcher())
-    //     .build();
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new GlobalCatcher())
+                .alwaysDo(print())
+                .build();
+        
+            final Instant now = Instant.now();
 
-    //     signInReq.setPassword("toto");
-    //     signInReq.setUsername("toto@xyz.fr");
+        signInReq.setPassword("toto");
+        signInReq.setUsername("toto@xyz.fr");
 
-    //     userEntity.setId("toto");
-    //     userEntity.setUsername("toto");
+        userEntity.setId("1234");
+        userEntity.setUsername("toto@xyz.fr");
+        userEntity.setPassword("toto");
 
-    //     signedInUser.setUserId("1234");
-    //     signedInUser.setUsername("toto@xyz.fr");
-    //     signedInUser.role("CUSTOMER");
-    // }
+        signedInUser.setUserId("1234");
+        signedInUser.setUsername("toto@xyz.fr");
+        signedInUser.role("CUSTOMER");
+    }
 
-    // @Test
-    // @DisplayName("signin with good credentials")
-    // public void signinWithNewUser() throws Exception {
-    //     given(service.findUserByEmail("toto", null))
-    //     .willReturn(userEntity);
+    @Test
+    @DisplayName("signin with good credentials")
+    public void signinWithExistingUser() throws Exception {
+        given(service.findUserByEmail("toto@xyz.fr", null))
+                .willReturn(userEntity);
 
-    //     given(passwordEncoder.matches(isA(String.class), isA(String.class)))
-    //     .willReturn(true);
+        given(passwordEncoder.matches(isA(CharSequence.class), isA(String.class)))
+                .willReturn(true);
 
-    //     given(service.getSignedInUser(userEntity))
-    //     .willReturn(signedInUser);
+        given(service.getSignedInUser(userEntity))
+                .willReturn(signedInUser);
 
-    //     //when
-    //     ResultActions result = mockMvc.perform(
-    //         get("/api/v1/auth/token")
-    //         .contentType(MediaType.APPLICATION_JSON)
-    //         .accept(MediaType.APPLICATION_JSON)
-    //         );
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(signInReq);
 
-    //     //then
-    //     result.andExpect(status().isOk())
-    //     .andExpect(jsonPath("userId", is(userEntity.getId())))
-    //     .andExpect(jsonPath("username", is(userEntity.getUsername())));
-    // }
-    
+        // when
+        ResultActions result = mockMvc.perform(
+                post("/api/v1/auth/token")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
+                System.out.println(" ------------------- " + result.toString());
+        // then
+        assertThat(controller).isNotNull();
+        System.out.println("------------- " + result.toString());
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("userId", is(userEntity.getId())))
+                .andExpect(jsonPath("username", is(userEntity.getUsername())));
+    }
+
 }
