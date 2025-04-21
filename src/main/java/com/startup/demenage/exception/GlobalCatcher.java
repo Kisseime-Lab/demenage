fda3da77dfe3fb2bdc79299d4474b8eda15e39a6
Kyse7535 <ktevot@gmail.com>
2025-04-21 10:58:19 +0200

    error-handling: creation new exceptions and methods to catch error

--- src/main/java/com/startup/demenage/exception/GlobalCatcher.java
+++ src/main/java/com/startup/demenage/exception/GlobalCatcher.java
@@ -2,20 +2,46 @@ package com.startup.demenage.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+
+import com.startup.demenage.model.ErrorApiModel;
 
 @ControllerAdvice
 public class GlobalCatcher {
-    
+
+    @ExceptionHandler({ ValidationInputException.class })
+    public ResponseEntity<ErrorApiModel> handle_validation_input_exception(Exception e) {
+        return new ResponseEntity<ErrorApiModel>(
+                new ErrorApiModel().code(HttpStatus.UNPROCESSABLE_ENTITY.name()).message(e.getMessage()),
+                HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler({ InvalidInputException.class, UsernameNotFoundException.class })
+    public ResponseEntity<ErrorApiModel> handle_invalid_input_exception(Exception e) {
+        return new ResponseEntity<ErrorApiModel>(
+                new ErrorApiModel().code(HttpStatus.BAD_REQUEST.name()).message(e.getMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ MethodArgumentNotValidException.class })
+    public ResponseEntity<ErrorApiModel> handle_illegal_argument_exception(MethodArgumentNotValidException e) {
+        return new ResponseEntity<ErrorApiModel>(
+                new ErrorApiModel()
+                        .code(HttpStatus.UNPROCESSABLE_ENTITY.name())
+                        .message(e.getFieldError().getField() + " " + e.getFieldError().getDefaultMessage()),
+                HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<?> handleException(Exception e, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(
-            HttpStatus.BAD_REQUEST.value(),
-            e.getMessage(),
-            request.getDescription(false)
-        );
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorApiModel> handleException(Exception e) {
+        ErrorApiModel errorDetails = new ErrorApiModel()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                .message(e.getMessage());
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
---
+++ src/main/java/com/startup/demenage/exception/ValidationInputException.java
@@ -0,0 +1,7 @@
+package com.startup.demenage.exception;
+
+public class ValidationInputException extends RuntimeException {
+    public ValidationInputException(String message) {
+        super(message);
+    }
+}
